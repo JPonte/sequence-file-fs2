4d4b2a2175c1784cb1db327898874ea68efd6bff
Joao Ponte <jpe@plista.com>
2022-03-10 17:17:15 +0100

    Sort out sbt stuff

--- build.sbt
+++ build.sbt
@@ -1,18 +1,26 @@
+enablePlugins(GitVersioning)
 
+git.gitTagToVersionNumber := { tag: String =>
+  if (tag matches "[0-9]+\\..*") Some(tag)
+  else None
+}
+git.useGitDescribe := true
 
-ThisBuild / version := "0.1.0-SNAPSHOT"
+name := "sequence_file_fs2"
+organization := "com.github.jponte"
+crossScalaVersions := Seq("2.12.15", "2.13.8", "3.1.1")
 
-ThisBuild / scalaVersion := "2.13.8"
+libraryDependencies ++= Seq(
+  "co.fs2" %% "fs2-core" % "3.2.5",
+  "co.fs2" %% "fs2-io" % "3.2.5",
+  "org.apache.hadoop" % "hadoop-client" % "3.3.1",
+  "org.apache.hadoop" % "hadoop-common" % "3.3.1",
+  "org.typelevel" %% "munit-cats-effect-3" % "1.0.7" % Test,
+)
 
-lazy val root = (project in file("."))
-  .settings(
-    name := "sequence_file_fs2",
-    libraryDependencies ++= Seq(
-      "co.fs2" %% "fs2-core" % "3.2.5",
-      "co.fs2" %% "fs2-io" % "3.2.5",
-      "org.apache.hadoop" % "hadoop-client" % "3.3.1",
-      "org.apache.hadoop" % "hadoop-common" % "3.3.1",
-
-      "org.typelevel" %% "munit-cats-effect-3" % "1.0.7" % Test,
-    )
-  )
+scalacOptions ++= Seq(
+  "-feature",
+  "-deprecation",
+  "-unchecked",
+  "-language:higherKinds"
+)
--- src/main/scala/sequence_fs2/SequenceFileFs2.scala
+++ src/main/scala/sequence_fs2/SequenceFileFs2.scala
@@ -8,9 +8,9 @@ import org.apache.hadoop.io.SequenceFile.{CompressionType, Reader, Writer}
 import org.apache.hadoop.io._
 
 import java.nio.ByteBuffer
-import scala.jdk.CollectionConverters.CollectionHasAsScala
+import scala.collection.JavaConverters.asScalaSetConverter
+import scala.language.higherKinds
 import scala.reflect.ClassTag
-
 
 object SequenceFileFs2 {
 
@@ -30,16 +30,17 @@ object SequenceFileFs2 {
     override def getValue(w: Writable): Unit = ()
   }
 
-  implicit val bytesWritableProvider: WritableProvider[ByteBuffer] = new WritableProvider[ByteBuffer] {
-    override def getEmptyWritable: Writable = new BytesWritable()
+  implicit val bytesWritableProvider: WritableProvider[ByteBuffer] =
+    new WritableProvider[ByteBuffer] {
+      override def getEmptyWritable: Writable = new BytesWritable()
 
-    override def getWritable(t: ByteBuffer): Writable = ???
+      override def getWritable(t: ByteBuffer): Writable = new BytesWritable(t.array())
 
-    override def getValue(w: Writable): ByteBuffer = {
-      val bw = w.asInstanceOf[BytesWritable]
-      ByteBuffer.wrap(bw.getBytes, 0, bw.getLength)
+      override def getValue(w: Writable): ByteBuffer = {
+        val bw = w.asInstanceOf[BytesWritable]
+        ByteBuffer.wrap(bw.getBytes, 0, bw.getLength)
+      }
     }
-  }
 
   implicit val intWritableProvider: WritableProvider[Int] = new WritableProvider[Int] {
     override def getEmptyWritable: Writable = new IntWritable()
@@ -89,25 +90,30 @@ object SequenceFileFs2 {
     override def getValue(w: Writable): String = w.asInstanceOf[Text].toString
   }
 
-  implicit def arrayWritableProvider[T: WritableProvider : ClassTag]: WritableProvider[Array[T]] = new WritableProvider[Array[T]] {
+  implicit def arrayWritableProvider[T: WritableProvider: ClassTag]: WritableProvider[Array[T]] =
+    new WritableProvider[Array[T]] {
 
-    override def getEmptyWritable: Writable = {
-      val innerWritable = implicitly[WritableProvider[T]].getEmptyWritable
-      new ArrayWritable(innerWritable.getClass)
+      override def getEmptyWritable: Writable = {
+        val innerWritable = implicitly[WritableProvider[T]].getEmptyWritable
+        new ArrayWritable(innerWritable.getClass)
+      }
+
+      override def getWritable(t: Array[T]): Writable = {
+        val innerWritableProvider = implicitly[WritableProvider[T]]
+        new ArrayWritable(
+          innerWritableProvider.getEmptyWritable.getClass,
+          t.map(innerWritableProvider.getWritable)
+        )
+      }
+
+      override def getValue(w: Writable): Array[T] = {
+        val innerWritable = implicitly[WritableProvider[T]]
+        w.asInstanceOf[ArrayWritable].get().map(innerWritable.getValue)
+      }
     }
 
-    override def getWritable(t: Array[T]): Writable = {
-      val innerWritableProvider = implicitly[WritableProvider[T]]
-      new ArrayWritable(innerWritableProvider.getEmptyWritable.getClass, t.map(innerWritableProvider.getWritable))
-    }
-
-    override def getValue(w: Writable): Array[T] = {
-      val innerWritable = implicitly[WritableProvider[T]]
-      w.asInstanceOf[ArrayWritable].get().map(innerWritable.getValue)
-    }
-  }
-
-  implicit def mapWritableProvider[K: WritableProvider : ClassTag, V: WritableProvider : ClassTag]: WritableProvider[Map[K, V]] = new WritableProvider[Map[K, V]] {
+  implicit def mapWritableProvider[K: WritableProvider: ClassTag, V: WritableProvider: ClassTag]
+      : WritableProvider[Map[K, V]] = new WritableProvider[Map[K, V]] {
     override def getEmptyWritable: Writable = new MapWritable()
 
     override def getWritable(t: Map[K, V]): Writable = {
@@ -124,34 +130,42 @@ object SequenceFileFs2 {
       val kProvider = implicitly[WritableProvider[K]]
       val vProvider = implicitly[WritableProvider[V]]
       val mapWritable = w.asInstanceOf[MapWritable]
-      mapWritable.keySet().asScala.map { k =>
-        kProvider.getValue(k) -> vProvider.getValue(mapWritable.get(k))
-      }.toMap
+      mapWritable
+        .keySet()
+        .asScala
+        .map { k =>
+          kProvider.getValue(k) -> vProvider.getValue(mapWritable.get(k))
+        }
+        .toMap
     }
   }
 
-  /**
-   * Creates an [[fs2.Stream]] that reads from the specified Sequence File.
-   * The key and values of the records need to have an implicit [[WritableProvider]] in scope, that can wrap its values
-   * into a hadoop Writable.
-   *
-   * @param filename   the file path of the Sequence File
-   * @param start      the number of bytes to skip
-   * @param length     the amount of bytes to read
-   * @param bufferSize the buffer size of the internal reader
-   * @param config     Hadoop Configuration for the internal reader
-   * @tparam F effect type
-   * @tparam K type of the record key
-   * @tparam V type of the record value
-   * @return [[fs2.Stream]] of records of type (K, V)
-   */
-  def read[F[_] : Sync, K: WritableProvider, V: WritableProvider]
-  (filename: String,
-   start: Option[Long] = None,
-   length: Option[Long] = None,
-   bufferSize: Option[Int] = None,
-   config: Configuration = new Configuration()): Stream[F, (K, V)] = {
-    val opts = List(Option(Reader.file(new Path(filename))), start.map(Reader.start), length.map(Reader.length), bufferSize.map(Reader.bufferSize)).flatten
+  /** Creates an [[fs2.Stream]] that reads from the specified Sequence File. The key and values of
+    * the records need to have an implicit [[WritableProvider]] in scope, that can wrap its values
+    * into a hadoop Writable.
+    *
+    * @param filename
+    *   the file path of the Sequence File
+    * @param bufferSize
+    *   the buffer size of the internal reader
+    * @param config
+    *   Hadoop Configuration for the internal reader
+    * @tparam F
+    *   effect type
+    * @tparam K
+    *   type of the record key
+    * @tparam V
+    *   type of the record value
+    * @return
+    *   [[fs2.Stream]] of records of type (K, V)
+    */
+  def read[F[_]: Sync, K: WritableProvider, V: WritableProvider](
+      filename: String,
+      bufferSize: Option[Int] = None,
+      config: Configuration = new Configuration()
+  ): Stream[F, (K, V)] = {
+    val opts =
+      List(Option(Reader.file(new Path(filename))), bufferSize.map(Reader.bufferSize)).flatten
     val acquireReader = Sync[F].blocking(new SequenceFile.Reader(config, opts: _*))
     Stream.bracket(acquireReader)(reader => Sync[F].blocking(reader.close())).flatMap { reader =>
       Stream.unfoldEval(reader) { _ =>
@@ -161,7 +175,12 @@ object SequenceFileFs2 {
           val kWritable = kWritableProvider.getEmptyWritable
           val vWritable = vWritableProvider.getEmptyWritable
           if (reader.next(kWritable, vWritable)) {
-            Option(((kWritableProvider.getValue(kWritable), vWritableProvider.getValue(vWritable)), reader))
+            Option(
+              (
+                (kWritableProvider.getValue(kWritable), vWritableProvider.getValue(vWritable)),
+                reader
+              )
+            )
           } else {
             None
           }
@@ -170,25 +189,40 @@ object SequenceFileFs2 {
     }
   }
 
-  /**
-   * Creates an [[fs2.Pipe]] that writes data to a single Sequence File.
-   * The key and values of the records need to have an implicit [[WritableProvider]] in scope, that can wrap its values
-   * into a hadoop Writable.
-   *
-   * @param filename        the file path of the Sequence File
-   * @param compressionType one of NONE, BLOCK, RECORD compression types
-   * @param config          Hadoop Configuration for the internal writer
-   * @tparam F effect type
-   * @tparam K type of the record key
-   * @tparam V type of the record value
-   * @return writer [[fs2.Pipe]]
-   */
-  def write[F[_] : Sync, K: WritableProvider, V: WritableProvider](filename: String, compressionType: CompressionType = CompressionType.NONE, config: Configuration = new Configuration()): Pipe[F, (K, V), (K, V)] = {
-    in => {
-      val acquireWriter = Sync[F].blocking(SequenceFile.createWriter(config, Writer.file(new Path(filename)),
-        Writer.keyClass(implicitly[WritableProvider[K]].getEmptyWritable.getClass),
-        Writer.valueClass(implicitly[WritableProvider[V]].getEmptyWritable.getClass),
-        Writer.compression(compressionType)))
+  /** Creates an [[fs2.Pipe]] that writes data to a single Sequence File. The key and values of the
+    * records need to have an implicit [[WritableProvider]] in scope, that can wrap its values into
+    * a hadoop Writable.
+    *
+    * @param filename
+    *   the file path of the Sequence File
+    * @param compressionType
+    *   one of NONE, BLOCK, RECORD compression types
+    * @param config
+    *   Hadoop Configuration for the internal writer
+    * @tparam F
+    *   effect type
+    * @tparam K
+    *   type of the record key
+    * @tparam V
+    *   type of the record value
+    * @return
+    *   writer [[fs2.Pipe]]
+    */
+  def write[F[_]: Sync, K: WritableProvider, V: WritableProvider](
+      filename: String,
+      compressionType: CompressionType = CompressionType.NONE,
+      config: Configuration = new Configuration()
+  ): Pipe[F, (K, V), (K, V)] = { in =>
+    {
+      val acquireWriter = Sync[F].blocking(
+        SequenceFile.createWriter(
+          config,
+          Writer.file(new Path(filename)),
+          Writer.keyClass(implicitly[WritableProvider[K]].getEmptyWritable.getClass),
+          Writer.valueClass(implicitly[WritableProvider[V]].getEmptyWritable.getClass),
+          Writer.compression(compressionType)
+        )
+      )
 
       Stream.bracket(acquireWriter)(writer => Sync[F].blocking(writer.close())).flatMap { writer =>
         val kWritableProvider = implicitly[WritableProvider[K]]
